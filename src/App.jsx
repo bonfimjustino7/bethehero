34a981c5fa713760e373ed8cd3bdcf8da1b85798
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-05-17 23:41:46 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logon from './pages/Logon';
+import './global.css';
+
+export default function App() {
+    return (
+        <Logon />
+    );
+}
---
+++ src/pages/Logon/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+import './style.css';
+import heroesImgs from '../../assets/heroes.png'
+import logoHeroes from '../../assets/logo.svg'
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoHeroes} alt="Logo" />
+
+                <form>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Sua ID" />
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImgs} alt="Heroes" />
+        </div>
+    );
+}
